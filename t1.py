3d147fd1dd41065e4f76f4506ce2ca7dbb90ccbb
ayushi <ayushiisingh0204@gmail.com>
2023-07-14 18:34:46 +0530

    loops

---
+++ t1.py
@@ -0,0 +1,25 @@
+from turtle import *
+
+speed('slowest')
+pencolor('red')
+pensize(3)
+
+#range(stop value)
+#range(start,stop)
+#range(start,stop,step)
+
+for i in range(1,20,2):
+    fd(90)
+    rt(360/10)
+    dot(10, 'Green')
+    write(i, font=('Calibre', 20, 'bold'))
+
+#reverse
+goto(100,100)
+for i in range(10,0,-1):
+    fd(60)
+    lt(360/10)
+    dot(20, 'red')
+    write(i, font=('Calibre', 20, 'bold'))
+       
+mainloop()
---
+++ t2.py
@@ -0,0 +1,16 @@
+from turtle import *
+
+speed('fastest')
+pencolor('blue')
+bgcolor('#303030')
+pensize(4)
+
+i = 100
+while i > 0:
+    fd(i)
+    lt(90)
+    i -= 4
+    dot(10,'red')
+    write(i, font=('Calibre', 20, 'bold'))
+
+mainloop() #holds 
